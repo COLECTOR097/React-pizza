342a5d6737ca61a76038b0dd1cb444f4db7c24cd
COLECTOR097 <ishankhanovsirojiddin@gmail.com>
2023-01-30 09:59:32 +0600

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header, Categories, Sort, PizzaBlock } from "./components";
+
+import "../src/scss/app.scss";
+
+import pizzas from "./assets/pizza.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((pizza, index) => {
+              return <PizzaBlock key={index} {...pizza} />;
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((category, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => setActiveIndex(index)}
+              className={activeIndex === index ? "active" : ""}
+            >
+              {category}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
